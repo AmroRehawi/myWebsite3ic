a0a7352e55c993831a34397cb01086d05f2caac8
Test User <amro.rehawi@lbs4.salzburg.at>
2023-03-02 16:33:04 +0100

    “02.03”

--- README.md
+++
@@ -1,2 +0,0 @@
-# myWebsite3ic
-Dieser Repo ist zum ständigen Update zwischen meinem Rechner und dem Raspberry Pi
---
+++ register.php
@@ -0,0 +1,11 @@
+<html>
+<h1>
+<?php
+if(isset($_POST['submit'])){
+$name = $_POST['familyName'];
+echo $name;}
+
+?>
+</h1>
+
+</html>
