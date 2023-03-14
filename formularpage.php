c2c941428befb027b42250c3938aae2a86ea88e3
Amro.Rehawi <amro.rehawi@lbs4.salzburg.at>
2023-03-14 21:21:48 +0100

    14.03

--- formularpage.php
+++ formularpage.php
@@ -1,29 +1,31 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <title>Formularpage</title>
+  <link rel="stylesheet" href="css/main.css">
 </head>
-<body>
-<!-- name is what you send to php, post ist the send method -->
-<form action="register.php" method = "post">
-  <label>Surname:</label>
-  <input type="text" name="surname" id="surname" ><br>
-  <label>Family name:</label>
-  <input type="text" name = "familyName" id="familyName" ><br>
+<body style ="background-color: #EAF6F6;" >
+<!-- name is what you send to php -->
 
-  <label>Email-Adress:</label>
-  <input type="email" name = "emailAddress" id="emailAddress"><br>
-  <!--
-  <label>Password:</label>
-  <input type="password" name = "password" id = "password"><br>
-  <label>Phone number:</label>
-  <input type ="number" name = "phoneNumber" id = "phoneNumber"><br> -->
-  <input type = "submit" name = "submit" id = "submit">
-
-
+<form class="formularPage-form" action="register.php" method = "post">
+    <label class="label">Surname:</label>
+  <input type="text" name="surname" id="surname" required minlength="4" maxlength="15" size="10"><br>
+  <label class="label">Family name:</label>
+  <input class="input" type="text" name = "familyName" id="familyName" required minlength="4" maxlength="15" size="10"><br>
+  <label class="label">Email-Adress:</label>
+  <input class="input" type="email" name = "emailAdress" id="emailAdresse"><br>
+  <label class="label">Password:</label>
+  <input class="input" type="password" name = "password" id = "password"><br>
+  <label class="label">Phone number:</label>
+  <input class="input" type ="number" name = "phoneNumber" id = "phoneNumber"><br>
+  <label class="label">Upload a profile image:</label>
+  <input type = "file" id="profilepic" name ="profilepic"><br>
+  <input class="input" type = "submit">
 </form>
+
+
+
 
 </body>
 </html>
--- register.php
+++ register.php
@@ -7,21 +7,24 @@ if(isset($_POST['submit'])) {
   $familyName = $_POST['familyName'];
   $surname = $_POST['surname'];
   $emailAddress = $_POST['emailAddress'];
-
+  $password = $_POST['password'];
+  $phoneNumber= $_POST['phoneNumber'];
   // öffnen der Datei im Appendmodus
   $filePointer = fopen($filename, "a");
 
+  $hashFormat = "2y$10$";
+  $salt = "iusesomecrazystringshaha26";
+  $hashFAndSalt = $hashFormat . $salt;
+  $password = crypt($password, $hashFAndSalt);
   // schreiben der Daten in die CSV-Datei
-  $data = array($surname,$familyName,$emailAddress);
+  $data = array($surname,$familyName,$phoneNumber,$emailAddress, $password );
   fputcsv($filePointer,$data);
   fclose($filePointer);
-
-  echo "im If \n";
 }
 $script_path = __FILE__;
 $file_path = dirname($script_path) . '/' . $filename;
 
-echo "außerhalb des iffs";
+
 
 ?></h1>
 
